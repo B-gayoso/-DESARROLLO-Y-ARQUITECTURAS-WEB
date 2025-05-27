3d6be54def144ba94bc04472506915c7f286eb4e
B-gayoso <biancaaylen.gayoso@alumnos.uai.edu.ar>
2025-05-26 21:37:08 -0300

    Variables y operadores

---
+++ Exercises/variables_and_operators.js
@@ -0,0 +1,15 @@
+// Ejercicio 1:
+let numero1 = 10;
+let numero2 = 25;
+let suma = numero1 + numero2;
+console.log("Resultado de la suma:", suma);
+// Ejercicio 2:
+let texto1 = "Bianca";
+let texto2 = "Gayoso";
+let concatenacion = texto1 + texto2;
+console.log("Resultado de la concatenación:", concatenacion);
+// Ejercicio 3:
+let palabra1 = "Desarrollo ";
+let palabra2 = "Web";
+let sumaDeLargos = palabra1.length + palabra2.length;
+console.log("Suma de los largos:", sumaDeLargos);
